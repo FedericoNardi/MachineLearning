fc9cc70b3f3e243e3c4f5a5191f21b25694570ab
Federico <federico.nardi@studenti.unitn.it>
2018-11-12 16:10:48 +0100

    Added DNN classifier

--- Project2/Project2_Logistic.py
+++ Project2/Project2_Logistic.py
@@ -201,17 +201,19 @@ num_classes=2
 train_to_test_ratio=0.01 # training samples
 
 # path to data directory
-path_to_data="C:\Anaconda\Programes/IsingData/"
+#path_to_data="C:\Anaconda\Programes/IsingData/"
 
 # load data
-file_name = "Ising2DFM_reSample_L40_T=All.pkl" # this file contains 16*10000 samples taken in T=np.arange(0.25,4.0001,0.25)
-data = pickle.load(open(path_to_data+file_name,'rb')) # pickle reads the file and returns the Python object (1D array, compressed bits)
+file_name = "IsingData/Ising2DFM_reSample_L40_T=All.pkl" # this file contains 16*10000 samples taken in T=np.arange(0.25,4.0001,0.25)
+#data = pickle.load(open(path_to_data+file_name,'rb'))
+data = pickle.load(open(file_name,'rb')) # pickle reads the file and returns the Python object (1D array, compressed bits)
 data = np.unpackbits(data).reshape(-1, 1600) # Decompress array and reshape for convenience
 data=data.astype('int')
 data[np.where(data==0)]=-1 # map 0 state to -1 (Ising variable can take values +/-1)
 
-file_name = "Ising2DFM_reSample_L40_T=All_labels.pkl" # this file contains 16*10000 samples taken in T=np.arange(0.25,4.0001,0.25)
-labels = pickle.load(open(path_to_data+file_name,'rb')) # pickle reads the file and returns the Python object (here just a 1D array with the binary labels)
+file_name = "IsingData/Ising2DFM_reSample_L40_T=All_labels.pkl" # this file contains 16*10000 samples taken in T=np.arange(0.25,4.0001,0.25)
+#slabels = pickle.load(open(path_to_data+file_name,'rb')) # pickle reads the file and returns the Python object (here just a 1D array with the binary labels)
+labels = pickle.load(open(file_name,'rb'))
 
 # divide data into ordered, critical and disordered
 X_ordered=data[:70000,:]
