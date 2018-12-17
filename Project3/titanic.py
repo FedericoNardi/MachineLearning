8b5812a5c90c847c376d6be132a22f82c91a6729
FrancescoSlongo <Slongofrancesco@gmail.com>
2018-12-17 08:52:34 +0100

    Update

--- Project3/titanic.py
+++ Project3/titanic.py
@@ -14,7 +14,6 @@ g.map(sns.distplot, 'Age', bins=20, norm_hist=True)
 plt.show()
 """
 
---------------------------------------------------------------------
 # Fill holes
 TrainData.info()
 
@@ -35,7 +34,6 @@ for i in range(len(index)):
 	TrainData['Embarked'][index[i]] = 'S'
 
 
-----------------------------------------------------------------------
 # Drop useless
 
 # Create Family size variable
@@ -48,12 +46,10 @@ TrainData.drop(['PassengerId','Name','SibSp','Parch','Ticket','Fare','Cabin'],ax
 TrainData.info()
 
 
----------------------------------------------------------------------
 # Convert categorical to int
 
 TrainData['Embarked'] = TrainData['Embarked'].map( {'S':0, 'C':1, 'Q':2} ).astype(np.int64)
 TrainData['Sex'] = TrainData['Sex'].map( {'male':0,'female':1} )
 #print(TrainData['Embarked'].head())
 
----------------------------------------------------------------------
 # SETTING UP REGRESSIONS
