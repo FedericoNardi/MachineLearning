78783947bec7bb45b9a38bfdeff01f3526f39f85
FrancescoSlongo <Slongofrancesco@gmail.com>
2018-12-17 10:59:15 +0100

    Update

--- Project3/titanic.py
+++ Project3/titanic.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,23 +18,43 @@ plt.show()
 # Fill holes
 TrainData.info()
 
-# Fill holes in Age
-NaNIndex = list(TrainData['Age'][TrainData['Age'].isnull()].index)
+# Most frequent port
+freq_port = TrainData.Embarked.dropna().mode()[0]
+print("The most frequent port is")
+print(freq_port)
 
-for i in range(len(NaNIndex)):
-	AgeMedian = TrainData['Age'].median()
-	AgePredict = TrainData["Age"][ ((TrainData['Parch'] == TrainData.iloc[i]["Parch"]) & (TrainData['Pclass'] == TrainData.iloc[i]["Pclass"]) ) ] .median()
-	if not np.isnan(AgePredict)==True:
-		TrainData['Age'][NaNIndex[i]] = AgePredict
-	else:
-		TrainData['Age'][NaNIndex[i]] = AgeMedian
+
 
 # Fill holes in Embarked
 index = TrainData['Embarked'][TrainData['Embarked'].isnull()].index
 for i in range(len(index)):	
 	TrainData['Embarked'][index[i]] = 'S'
+#%%
+# Convert categorical to int
 
+TrainData['Embarked'] = TrainData['Embarked'].map( {'S':0, 'C':1, 'Q':2} ).astype(np.int64)
+TrainData['Sex'] = TrainData['Sex'].map( {'male':0,'female':1} )
 
+## Correlation matrix between numerical values (SibSp Parch Age and Fare values) and Survived 
+#sns.heatmap(TrainData[["Age","SibSp","Parch","Pclass", "Sex","Fare","Embarked"]].corr(),annot=True, fmt = ".2f", cmap = "coolwarm")
+
+# Explore Age vs Survived
+#g = sns.FacetGrid(TrainData, col='Survived')
+#g.map(plt.hist, 'Age', bins=20)
+
+#sns.factorplot(y="Age",x="Pclass", data=TrainData,kind="box")
+#sns.factorplot(y="Age",x="SibSp", data=TrainData,kind="box")
+# Fill holes in Age
+NaNIndex = list(TrainData['Age'][TrainData['Age'].isnull()].index)
+
+for i in range(len(NaNIndex)):
+	AgeMedian = TrainData['Age'].mean()
+	AgePredict = TrainData["Age"][ ((TrainData['Parch'] == TrainData.iloc[i]["Parch"]) & (TrainData['Pclass'] == TrainData.iloc[i]["Pclass"]) ) ] .median()
+	if not np.isnan(AgePredict)==True:
+		TrainData['Age'][NaNIndex[i]] = AgePredict
+	else:
+		TrainData['Age'][NaNIndex[i]] = AgeMedian
+#%%
 # Drop useless
 
 # Create Family size variable
@@ -41,15 +62,37 @@ Family = TrainData['SibSp']+TrainData['Parch']
 TrainData['Family'] = Family
 
 #Drop not relevant data
-TrainData.drop(['PassengerId','Name','SibSp','Parch','Ticket','Fare','Cabin'],axis=1,inplace=True)
+TrainData.drop(['PassengerId','Name','SibSp','Parch','Ticket','Cabin'],axis=1,inplace=True)
 
 TrainData.info()
 
+## Correlation matrix between numerical values (SibSp Parch Age and Fare values) and Survived 
+#sns.heatmap(TrainData[["Survived","Age","Family","Pclass", "Sex","Fare","Embarked"]].corr(),annot=True, fmt = ".2f", cmap = "coolwarm")
 
-# Convert categorical to int
+g = sns.factorplot(x="Sex",y="Survived",data=TrainData,kind="bar", size = 6 , 
+palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels("survival probability")
 
-TrainData['Embarked'] = TrainData['Embarked'].map( {'S':0, 'C':1, 'Q':2} ).astype(np.int64)
-TrainData['Sex'] = TrainData['Sex'].map( {'male':0,'female':1} )
+# Explore Pclass vs Survived
+g = sns.factorplot(x="Pclass",y="Survived",data=TrainData,kind="bar", size = 6 , 
+palette = "muted")
+g.despine(left=True)
+g = g.set_ylabels("survival probability")
+
+# Explore Embarked vs Survived 
+g = sns.factorplot(x="Embarked", y="Survived",  data=TrainData,
+                   size=6, kind="bar", palette="muted")
+g.despine(left=True)
+g = g.set_ylabels("survival probability")
+
+# Explore Embarked vs Survived 
+g = sns.factorplot(x="Family", y="Survived",  data=TrainData,
+                   size=6, kind="bar", palette="muted")
+g.despine(left=True)
+g = g.set_ylabels("survival probability")
+
+#%%
 #print(TrainData['Embarked'].head())
 
 # SETTING UP REGRESSIONS
